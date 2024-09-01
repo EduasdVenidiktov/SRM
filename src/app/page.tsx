b04464ba670c1aa1aa16d3a00a551303ee88e6fb
Eduard Venidiktov <edven7777777@gmail.com>
2024-09-01 16:56:43 +0300

    load Formik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import StatusLabel, { Status } from '@/app/components/status-label';
+import {headers} from "next/headers";
 
 export default function Home() {
-  return (
-      <main>
-        <h1 className="text-xl">Home page</h1>
-        <StatusLabel status={Status.Active}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-      </main>
-  );
+    console.log(headers());
+
+    return (
+        <main>
+            <h1 className="text-xl">Home page {new Date().toLocaleTimeString()}</h1>
+            <StatusLabel status={Status.Active}>Active</StatusLabel>
+            <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+            <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+            <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+        </main>
+    );
 }
